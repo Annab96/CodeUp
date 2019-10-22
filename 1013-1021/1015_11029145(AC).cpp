b90bfa24fd84ba779f33aeaba530ba7a794db25c
annab96 <lannab082@gmail.com>
2019-10-23 07:35:25 +0900

    191023 [CodeUp] 1013-1021

---
+++ 1013-1021/1015_11029145(AC).cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main(){
+    float f;
+    scanf("%f", &f);
+    printf("%.2f", f);
+    
+    return 0;
+}
---
+++ 1013-1021/1017_11029184(AC).cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(){
+    int n;
+    int i;
+    
+    scanf("%d", &n);
+    for(i=0; i<3; i++)
+        printf("%d ", n);
+        
+    return 0;
+}
