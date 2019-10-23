40ce56464fa7e17bc5a51ac4e7db6d21da082f98
annab96 <lannab082@gmail.com>
2019-10-23 09:54:46 +0900

    191023 [CodeUp] 1022-1030

---
+++ 1022-1030/1022_11037009(AC).cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main(){
+    char str[2001];
+    fgets(str, 2000, stdin);
+    printf("%s", str);
+    
+    return 0;
+}
---
+++ 1022-1030/1024_11037251(AC).cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main(){
+    char word[20];
+    scanf("%s", word);
+    for(int i = 0 ; word[i]!='\0';i++)
+        printf("\'%c\'\n", word[i]);
+    return 0;
+}
