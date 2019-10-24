49a2ddb688ee73779e0db4a1f6f83510783bbe4c
annab96 <lannab082@gmail.com>
2019-10-24 10:54:59 +0900

    191024 [CodeUp] 1081-1090

---
+++ 1081-1090/1084_11052585(AC).cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(){
+    int a,b,c,i,j,k;
+    scanf("%d %d %d", &a, &b, &c);
+    for(i=0;i<a;i++)
+        for(j=0;j<b;j++)
+            for(k=0;k<c;k++)
+                printf("%d %d %d\n", i, j, k);
+    printf("%d", a*b*c);
+	return 0;
+}
---
+++ 1081-1090/1090_11053771(AC).cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(){
+    long int a, r, n, i;
+    scanf("%ld %ld %ld", &a, &r, &n);
+    for(i=0;i<n-1;i++)
+        a*=r;
+    printf("%ld", a);
+	return 0;
+}
