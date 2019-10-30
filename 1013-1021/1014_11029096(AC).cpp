7f66d7c6d743d90855cf5f982d02997720a7fc2e
annab96 <lannab082@gmail.com>
2019-10-30 14:31:19 +0900

    191030 [CodeUp] 그리디 2001

--- 1013-1021/1014_11029096(AC).cpp
+++ 1013-1021/1014_11029096(AC).cpp
@@ -1,9 +1,30 @@
-﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(){
-    char a,b;
-    scanf("%c %c", &a, &b);
-    printf("%c %c", b, a);
-    
-    return 0;
+int main(void) {
+    int n, i, j;
+    int a[1001] = { 0 };
+    cin >> n;
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int m, tmp = 0, index = 0;
+    for (i = 0; i < n - 1; i++) {
+        m = a[i];
+        index = i;
+        for (j = i + 1; j < n; j++) {
+            if (m > a[j]) {
+                m = a[j];
+                index = j;
+            }
+        }
+        tmp = a[i];
+        a[i] = m;
+        a[index] = tmp;
+    }
+    int sum=0;
+    for (i = n; i > 0; i--)
+        for (j = 0; j < i; j++)
+            sum += a[j];
+    cout << sum;
 }
---
+++ 그리디/2001_11133102(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void){
+    int a[3];
+    int b[2];
+    cin>>a[0]>>a[1]>>a[2];
+    cin>>b[0]>>b[1];
+    int m1, m2;
+    m1 = (((a[0]>a[1])?a[1]:a[0])>a[2])?a[2]:(a[0]>a[1]?a[1]:a[0]);
+    m2 = b[0]>b[1]?b[1]:b[0];
+    float sum = ((float)m1+(float)m2)*1.1;
+    cout << fixed;
+    cout.precision(1);
+    cout<<sum;
+	return 0;
+}
